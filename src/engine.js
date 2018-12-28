dcf15faa75324f5b7fc29893b07b72ee10cd6ecb
igbes <igbes900@gmail.com>
2018-12-28 20:06:19 +0800

    added changes

--- src/engine.js
+++ src/engine.js
@@ -1,28 +1,23 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".');
-
-const userName = readlineSync.question('May I have your name?: ');
-console.log(`Hello, ${userName}!`);
-
-const randomInteger = () => {
-  const min = 1;
-  const max = 100;
+const randomInteger = (min = 1, max = 100) => {
   const rand = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(rand);
 };
 
 const isEven = num => num % 2 === 0;
 
-const rightAnswer = (n) => {
-  const res = isEven(n) ? 'yes' : 'no';
-  return res;
-};
+const rightAnswer = n => (isEven(n) ? 'yes' : 'no');
 
 const numSteps = 3;
 
 const getEven = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".');
+
+  const userName = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${userName}!`);
+
   const iter = (count) => {
     if (count === 0) {
       console.log(`Congratulations, ${userName}!`);
@@ -34,12 +29,8 @@ const getEven = () => {
     if (answer === rightAnswer(questionExpression)) {
       console.log('Correct!');
       iter(count - 1);
-    } else if (answer === 'yes') {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-    } else if (answer === 'no') {
-      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
     } else {
-      console.log("Incorrect value! Enter 'yes' other 'no' only!");
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer(questionExpression)}'.`);
     }
   };
   return iter(numSteps);
