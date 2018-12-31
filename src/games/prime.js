46afee1a438ca392de66ba1d40bb81a133175807
igbes <igbes900@gmail.com>
2018-12-31 16:16:50 +0800

    added changes

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,7 @@ const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
   const iter = (num) => {
-    if (num < 2) return true;
+    if (num === 1) return true;
     if (number % num === 0) return false;
     return iter(num - 1);
   };
@@ -16,7 +16,7 @@ const yesOrNo = num => (isPrime(num) ? 'yes' : 'no');
 
 const prime = () => {
   const randomNumber = randomInteger(1, 100);
-  const rightAnswer = String(yesOrNo(randomNumber));
+  const rightAnswer = yesOrNo(randomNumber);
   const question = (`${randomNumber}`);
   return [rightAnswer, question];
 };
