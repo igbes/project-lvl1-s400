9653061a5f5e0622dad271376374d904ce759823
igbes <igbes900@gmail.com>
2018-12-31 18:20:57 +0800

    added changes

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,12 +5,14 @@ const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
   if (number < 2) return false;
+  const lastElem = Math.floor(Math.sqrt(number));
   const iter = (num) => {
-    if (num === 1) return true;
+    if (num === lastElem + 1 && lastElem > 2 ) return true;
+    if (num === number) return true;
     if (number % num === 0) return false;
-    return iter(num - 1);
+    return iter(num + 1);
   };
-  return iter(number - 1);
+  return iter(2);
 };
 
 const yesOrNo = num => (isPrime(num) ? 'yes' : 'no');
