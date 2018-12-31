9a713d88f6fd28c51789d25a9199e9e80b144551
igbes <igbes900@gmail.com>
2018-12-31 16:27:09 +0800

    added changes

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,6 +4,7 @@ import engine from '../game-engine';
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
+  if (number < 2) return false;
   const iter = (num) => {
     if (num === 1) return true;
     if (number % num === 0) return false;
